683013cf2129dcc520a81917ebd1bd5f0773d40e
technomics <remya@technomicssolutions.com>
2014-10-01 11:09:04 +0530

    url added

--- cadd/cadd/urls.py
+++ cadd/cadd/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, include, url
+from djnago.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cadd.views.home', name='home'),
-    # url(r'^cadd/', include('cadd.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('web.urls')),
+	url(r'^college/', include('college.urls')),
+	url(r'^admission/', include('admission.urls')),
+	url(r'^staff/', include('staff.urls')),
+	url(r'^fees/', include('fees.urls')),
+	url(r'^attendance/', include('attendance.urls')),
+	url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
