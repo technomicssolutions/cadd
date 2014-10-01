a8f028ebc07e1d13f2cc371b5eaca97f66d4eee9
Anoop Kumar <anoopkumar@technomicssolutions.com>
2014-10-01 12:17:50 +0530

    collge views

--- cadd/college/views.py
+++ cadd/college/views.py
@@ -6,11 +6,24 @@ from django.views.generic.base import View
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
+from college.models import Software
 
 class ListSoftwares(View):
 
-	def get(self, request, *args, **kwargs):
-    
-    		return render(request, 'college/list_softwares.html', {})
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            software_list = []
+            softwares = Software.objects.all()
+            for software in softwares:
+                software_list.append({
+                    'id': software.id,
+                    'name': software.name,
+                    })
+            res = {
+                'softwares': software_list,
+            }
+            response = simplejson.dumps(res)
+            return HttpResponse(response, status=status, mimetype='application/json')
+        return render(request, 'list_softwares.html', {})
 
 
