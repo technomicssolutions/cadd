7cb0ddf1f5b7e1a4c75703bc1531d3f244c0efce
geethu <geethu@technomicssolutions.com>
2014-10-03 11:08:45 +0530

    Added fees details in Admission

--- cadd/admission/models.py
+++ cadd/admission/models.py
@@ -3,33 +3,45 @@ from django.db import models
 from college.models import Course, Batch
 
 
+class Installment(models.Model):
+
+	due_date = models.DateField('Due Date', null=True, blank=True)
+	amount = models.DecimalField('Amount',max_digits=14, decimal_places=2, default=0)
+	fine_amount = models.DecimalField('Fine Amount',max_digits=14, decimal_places=2, default=0)
+	
+	def __unicode__(self):
+
+		return str(self.amount)
+	
+	class Meta:
+
+		verbose_name_plural = 'Installment'
+
 class Student(models.Model):
+
 	student_name = models.CharField('Student Name', null=True, blank=True, max_length=200)
-	roll_number = models.IntegerField('Roll Number', default=0, null=True, blank=True)
+	roll_number = models.CharField('Roll Number', null=True, blank=True, max_length=200)
+	unique_id = models.CharField('Unique ID', null=True, blank=True, max_length=200)
+	cadd_registration_no = models.CharField('Cadd Registration No', null=True, blank=True, max_length=200)
 	address = models.CharField('Student Address', null=True, blank=True, max_length=200 )
 	course = models.ForeignKey(Course, null=True, blank=True)
 	batch = models.ForeignKey(Batch, null=True, blank=True)
 	dob = models.DateField('Date of Birth',null=True, blank=True)
-	address= models.CharField('Student Address',null=True, blank=True, max_length=200)
 	mobile_number= models.CharField('Mobile Number',null=True, blank=True, max_length=200)
-	land_number= models.CharField('Land Number',null=True, blank=True, max_length=200)
 	email = models.CharField('Email',null=True, blank=True, max_length=200)
 	blood_group = models.CharField('Blood Group',null=True, blank=True, max_length=200)
 	doj = models.DateField('Date of Join',null=True, blank=True)
 	photo = models.ImageField(upload_to = "uploads/photos/", null=True, blank=True)
+	qualifications = models.TextField('Qualifications', null=True, blank=True)
 	certificates_submitted = models.CharField('Certificates',null=True, blank=True, max_length=200)
-	certificates_remarks = models.CharField('Remarks',null=True, blank=True, max_length=200)
-	certificates_file = models.CharField('File Number',null=True, blank=True, max_length=200)
 	id_proofs_submitted = models.CharField('Id Proofs',null=True, blank=True, max_length=200)
-	id_proofs_remarks = models.CharField('Remarks',null=True, blank=True, max_length=200)
-	id_proofs_file = models.CharField('File Number',null=True, blank=True, max_length=200)
 	guardian_name = models.CharField('Guardian Name',null=True, blank=True, max_length=200)
-	guardian_address= models.CharField('Guardian Address',null=True, blank=True, max_length=200)
 	relationship = models.CharField('Relationship',null=True, blank=True, max_length=200)
 	guardian_mobile_number= models.CharField('Guardian Mobile Number',null=True, blank=True, max_length=200)
-	guardian_land_number= models.CharField('Guardian Land Number',null=True, blank=True, max_length=200)
-	guardian_email = models.CharField('Guardian Email',null=True, blank=True, max_length=200)
 	is_rolled = models.BooleanField('Is Rolled',default=False)
+	fees = models.DecimalField('Fees', max_digits=14, decimal_places=2, default=0)
+	no_installments = models.IntegerField('No of Installments', default=0)
+	installments = models.ManyToManyField(Installment, null=True, blank=True)
 
 	def __unicode__(self):
 		return str(self.student_name)
