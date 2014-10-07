42ffb3066e86cfd5f1f811226e3a5ebd5fb898a7
Anoop Kumar <anoopkumar@technomicssolutions.com>
2014-10-07 12:38:57 +0530

    letter

--- cadd/web/models.py
+++ cadd/web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Letter(models.Model):
+
+	letter_type = models.CharField('Letter Type',null=True, blank=True, max_length=200)
+	date = models.DateField('Date',null=True, blank=True)
+	to_address = models.CharField('To Address',null=True, blank=True, max_length=200) 
+	from_address = models.CharField('From Address',null=True, blank=True, max_length=200)
+
+	def __unicode__(self):
+		return self.letter_type + str('') + self.from_address
--- cadd/web/urls.py
+++ cadd/web/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
-from web.views import *
+from web.views import Home, ResetPassword, Login, Logout, Letters, DeleteLetter
 
 urlpatterns = patterns('',
 	
 	url(r'^$', Home.as_view(), name='home'),
 	url(r'login/$',  Login.as_view(), name='login'),
     url(r'logout/$', Logout.as_view(), name='logout'),
+
+	url(r'letters/$', Letters.as_view(), name='letters'),    
+	url(r'^delete_letter/(?P<letter_id>\d+)/$',login_required (DeleteLetter.as_view()), name='delete_letter'),
     
     url(r'^reset_password/(?P<user_id>\d+)/$', login_required(ResetPassword.as_view()), name="reset_password"),
 	
